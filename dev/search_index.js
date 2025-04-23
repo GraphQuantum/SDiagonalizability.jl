ec853fd8f2398373605a82d8a83b31f63dd6999b
Documenter.jl <documenter@juliadocs.github.io>
2025-04-23 19:44:58 +0000

    build based on f22ea0e

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SDiagonalizability","page":"Home","title":"SDiagonalizability","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SDiagonalizability.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
