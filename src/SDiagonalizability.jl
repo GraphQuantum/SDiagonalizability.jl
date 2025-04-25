2e6c495e4643807c668e12b3c34a02e824d99397
Luis-Varona <lbvarona@mta.ca>
2025-04-25 07:18:18 -0300

    Fix formatting; update dependencies; enrich comments
    Formatted files with JuliaFormatter using BlueStyle instead of Default Style. Also, added `RowEchelon.jl` as a dependency in the Project.toml (and imported it in src/SDiagonalibility.jl), for use in laplacian_spectra. Finally, made some edits to the inline comments in eigenvector_generators.

--- src/SDiagonalizability.jl
+++ src/SDiagonalizability.jl
@@ -1,10 +1,11 @@
 module SDiagonalizability
 
 using Combinatorics: combinations, multiset_permutations
-using DataStructures: counter
+using DataStructures: OrderedDict, counter
 using Graphs: SimpleGraph, is_bipartite, is_connected
 using LinearAlgebra
 using MolecularGraph: nodesubgraph_is_isomorphic
+using RowEchelon: rref_with_pivots
 using PrecompileTools: @setup_workload, @compile_workload
 
 end
--- src/SDiagonalizability/eigenvector_generators.jl
+++ src/SDiagonalizability/eigenvector_generators.jl
@@ -26,25 +26,31 @@ julia> collect(Vector{Int}, _potential_kernel_eigvecs(2))
 ```
 """
 function _potential_kernel_eigvecs(n::Int)
-    # Create caches for reusing the `leading` and `entries` vectors
+    # Cache to avoid redundant recomputations of the `leading` vector
     leading_cache = Dict{Int,Vector{Int}}()
 
     return (
-        vcat(get!(leading_cache, k) do
-            leading = Vector{Int}(undef, k)
-            leading[1:(k-1)] .= 0
-            leading[k] = 1 # Normalize the leading entry to 1
-            leading
-        end, body) # Append the permuted entries to the leading [0, ..., 0, 1] vector
-        for k = 1:n # Iterate over possible indices of first nonzero entry
+        vcat(
+            get!(leading_cache, k) do
+                leading = Vector{Int}(undef, k)
+                leading[1:(k - 1)] .= 0
+                leading[k] = 1 # Normalize the leading entry to 1
+                leading
+            end,
+            body,
+        ) # Append the permuted entries to the leading [0, ..., 0, 1] vector
+        for k in 1:n # Iterate over possible indices of first nonzero entry
         # Iterate over permutations taken from the `r`th Cartesian power of {-1,0,1}
-        for body in multiset_permutations(let r = n - k
-            entries = Vector{Int}(undef, 3r)
-            entries[1:r] .= -1
-            entries[(r+1):2r] .= 0
-            entries[(2r+1):3r] .= 1
-            entries
-        end, n - k)
+        for body in multiset_permutations(
+            let r = n - k
+                entries = Vector{Int}(undef, 3r)
+                entries[1:r] .= -1
+                entries[(r + 1):2r] .= 0
+                entries[(2r + 1):3r] .= 1
+                entries
+            end,
+            n - k,
+        )
     )
 end
 
@@ -84,28 +90,34 @@ julia> collect(Vector{Int}, _potential_nonkernel_eigvecs(4))
 ```
 """
 function _potential_nonkernel_eigvecs(n::Int)
-    # Create caches for reusing the `leading` and `entries` vectors
+    # Caches to avoid redundant recomputations of the `leading` and `entries` vectors
     leading_cache = Dict{Int,Vector{Int}}()
     entries_cache = Dict{Int,Vector{Int}}()
     entries_key = 0
 
     return (
-        vcat(get!(leading_cache, k) do
-            leading = Vector{Int}(undef, k)
-            leading[1:(k-1)] .= 0
-            leading[k] = 1 # Normalize the leading entry to 1
-            leading
-        end, body) # Append the permuted entries to the leading [0, ..., 0, 1] vector
-        for k = 1:n # Iterate over indices of the first nonzero entry
+        vcat(
+            get!(leading_cache, k) do
+                leading = Vector{Int}(undef, k)
+                leading[1:(k - 1)] .= 0
+                leading[k] = 1 # Normalize the leading entry to 1
+                leading
+            end,
+            body,
+        ) # Append the permuted entries to the leading [0, ..., 0, 1] vector
+        for k in 1:n # Iterate over indices of the first nonzero entry
         # Iterate over combinations of the remaining `r` entries, varying the number of -1's
-        for j = 1:ceil(Int, (r=n-k)/2)
+        for j in 1:ceil(Int, (r = n - k) / 2)
         # Iterate over permutations of the `entries` combination with `j` -1's
-        for body in multiset_permutations(get!(entries_cache, entries_key += 1) do
-            entries = Vector{Int}(undef, r)
-            entries[1:j] .= -1
-            entries[(j+1):(r-j+1)] .= 0
-            entries[(r-j+2):r] .= 1
-            entries
-        end, r)
+        for body in multiset_permutations(
+            get!(entries_cache, entries_key += 1) do
+                entries = Vector{Int}(undef, r)
+                entries[1:j] .= -1
+                entries[(j + 1):(r - j + 1)] .= 0
+                entries[(r - j + 2):r] .= 1
+                entries
+            end,
+            r,
+        )
     )
 end
