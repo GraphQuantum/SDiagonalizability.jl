e67aa24125d4a221587eb64c33d741bb5779eab8
Luis-Varona <lbvarona@mta.ca>
2025-04-29 01:27:20 -0300

    Finish logic for laplacian_spectra
    Finished the logic and API for src/laplacian_spectra.jl. Will add docstrings with doctests in a future commit. Furthermore, added a more description/definition of S-bandwidth to the README, and included a Credits section.

--- src/SDiagonalizability.jl
+++ src/SDiagonalizability.jl
@@ -2,10 +2,10 @@ module SDiagonalizability
 
 using Combinatorics: combinations, multiset_permutations
 using DataStructures: OrderedDict, counter
+using ElasticArrays: ElasticMatrix
 using Graphs: SimpleGraph, is_bipartite, is_connected
 using LinearAlgebra
 using MolecularGraph: nodesubgraph_is_isomorphic
-using RowEchelon: rref_with_pivots
 using PrecompileTools: @setup_workload, @compile_workload
 
 include("error_messages.jl")
