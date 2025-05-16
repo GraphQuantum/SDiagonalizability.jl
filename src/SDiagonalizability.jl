11bfd5eceed9b16e8b89b92e520ea4ff6e06d4f2
Luis M. B. Varona <lbvarona@mta.ca>
2025-05-15 23:51:29 -0300

    Refactor existing API (#16)
    Extract some errors/asserts into src/utils.jl. Make src/laplacian_types.jl to encapsulate logic on typing Laplacian matrices (null graph, empty graph, complete graph, general case) to support a factory design pattern in src/laplacian_spectra leveraging spectral properties of different graph types. Improve on some doctests and documentation in src/eigenvector_generation.jl. Rename several other files to be more idiomatic (e.g., src/k_orthogonalizability.jl -> src/basis_search.jl). Add more explicit TODO's throughout the codebase.

--- src/SDiagonalizability.jl
+++ src/SDiagonalizability.jl
@@ -6,18 +6,23 @@
 
 module SDiagonalizability
 
+using Graphs
+using LinearAlgebra
+
 using Combinatorics: combinations, multiset_permutations
 using DataStructures: OrderedDict, counter
 using ElasticArrays: ElasticMatrix
-using Graphs: SimpleGraph, is_bipartite, is_connected
-using LinearAlgebra
 using MolecularGraph: nodesubgraph_is_isomorphic
+
 using PrecompileTools: @setup_workload, @compile_workload
 
-include("error_messages.jl")
-include("eigenvector_generators.jl")
+include("utils.jl")
+include("laplacian_types.jl")
+include("eigenvector_generation.jl")
 include("laplacian_spectra.jl")
-include("k_orthogonalizability.jl")
+include("basis_search.jl")
 include("s_bandwidth.jl")
+
+# TODO: Add exports
 
 end
