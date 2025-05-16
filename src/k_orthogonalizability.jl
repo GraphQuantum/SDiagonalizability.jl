69b7ebf4b6cc834ed7f6eb4f925fd7b656419ed4
Luis M. B. Varona <lbvarona@mta.ca>
2025-05-15 23:51:29 -0300

    Refactor existing API (#16)
    Extract some errors/asserts into src/utils.jl. Make src/laplacian_types.jl to encapsulate logic on typing Laplacian matrices (null graph, empty graph, complete graph, general case) to support a factory design pattern in src/laplacian_spectra leveraging spectral properties of different graph types. Improve on some doctests and documentation in src/eigenvector_generation.jl. Rename several other files to be more idiomatic (e.g., src/k_orthogonalizability.jl -> src/basis_search.jl). Add more explicit TODO's throughout the codebase.

--- src/k_orthogonalizability.jl
+++
@@ -1,7 +0,0 @@
-# Copyright 2025 Luis M. B. Varona and Nathaniel Johnston
-#
-# Licensed under the MIT license <LICENSE or
-# http://opensource.org/licenses/MIT>. This file may not be copied, modified, or
-# distributed except according to those terms.
-
-
