6a9dcfcc24322fe999559849a78d1f870c7cb05f
Luis M. B. Varona <lbvarona@mta.ca>
2025-07-31 15:22:15 -0300

    Core logic all done; just need complete graph checks (#20)
    We are all done with the core API of the package, but some checks for complete graphs with their huge eigenspaces need to be implemented. Johnston and Plosker (2025)'s Theorem 1 will help for {-1,1}-bandwidths.

--- src/core.jl
+++ src/core.jl
@@ -5,53 +5,140 @@
 # distributed except according to those terms.
 
 """
-    minimize_s_bandwidth(g::AbstractGraph, S)
-    minimize_s_bandwidth(L::AbstractMatrix{<:Integer}, S)
+    minimize_s_bandwidth(g::AbstractGraph, S) -> SBandMinimizationResult
+    minimize_s_bandwidth(L::AbstractMatrix{<:Integer}, S) -> SBandMinimizationResult
 
 [TODO: Write here]
 """
 function minimize_s_bandwidth(g::AbstractGraph, S::Tuple{Vararg{Integer}})
     _assert_graph_has_defined_s_bandwidth(g)
-    return minimize_s_bandwidth(laplacian_matrix(g), S)
+    res = minimize_s_bandwidth(laplacian_matrix(g), S)
+    return SBandMinimizationResult(copy(g), res.S, res.diagonalization, res.band)
 end
 
 function minimize_s_bandwidth(L::AbstractMatrix{<:Integer}, S::Tuple{Vararg{Integer}})
     _assert_matrix_is_undirected_laplacian(L)
-    # TODO: Write here
-    return nothing
+
+    spec = laplacian_s_spectra(L, S)
+
+    if !spec.s_diagonalizable
+        return SBandMinimizationResult(copy(L), S, nothing, Inf)
+    end
+
+    pre_diagonalization = hcat(values(spec.s_eigenbases)...)
+    # `MatrixBandwidth.jl` uses zero-based indexing, not one-based, so we add 1 here
+    band_max = bandwidth(pre_diagonalization'pre_diagonalization) + 1
+
+    k_diag_found = false
+    band = 1
+
+    while (!k_diag_found && band < band_max)
+        res_temp = _s_spectra_has_bandwidth_at_most_k(spec, band)
+
+        if res_temp.has_band_k_diag
+            k_diag_found = true
+            diagonalization = res_temp.diagonalization
+        else
+            band += 1
+        end
+    end
+
+    if !k_diag_found
+        diagonalization = pre_diagonalization
+    end
+
+    return SBandMinimizationResult(copy(L), S, diagonalization, band)
 end
 
 """
-    has_s_bandwidth_at_most_k(g::AbstractGraph, S, k)
-    has_s_bandwidth_at_most_k(L::AbstractMatrix{<:Integer}, S, k)
+    has_s_bandwidth_at_most_k(g::AbstractGraph, S, k) -> SBandRecognitionResult
+    has_s_bandwidth_at_most_k(L::AbstractMatrix{<:Integer}, S, k) -> SBandRecognitionResult
 
 [TODO: Write here]
 """
 function has_s_bandwidth_at_most_k(g::AbstractGraph, S::Tuple{Vararg{Integer}}, k::Integer)
     _assert_graph_has_defined_s_bandwidth(g)
-    return has_s_bandwidth_at_most_k(laplacian_matrix(g), S, k)
+    res = has_s_bandwidth_at_most_k(laplacian_matrix(g), S, k)
+    return SBandRecognitionResult(
+        copy(g), res.S, res.diagonalization, k, res.has_band_k_diag
+    )
 end
 
 function has_s_bandwidth_at_most_k(
     L::AbstractMatrix{<:Integer}, S::Tuple{Vararg{Integer}}, k::Integer
 )
     _assert_matrix_is_undirected_laplacian(L)
-    # TODO: Write here
-    return nothing
+    return _s_spectra_has_bandwidth_at_most_k(laplacian_s_spectra(L, S), k)
 end
 
 """
-    is_s_diagonalizable(g::AbstractGraph, S)
-    is_s_diagonalizable(L::AbstractMatrix{<:Integer}, S)
+    is_s_diagonalizable(g::AbstractGraph, S) -> SBandRecognitionResult
+    is_s_diagonalizable(L::AbstractMatrix{<:Integer}, S) -> SBandRecognitionResult
 
 [TODO: Write here]
 """
 function is_s_diagonalizable(g::AbstractGraph, S::Tuple{Vararg{Integer}})
     _assert_graph_has_defined_s_bandwidth(g)
-    return is_s_diagonalizable(laplacian_matrix(g), S)
+    res = is_s_diagonalizable(laplacian_matrix(g), S)
+    return SBandRecognitionResult(
+        copy(g), res.S, res.diagonalization, nv(g), res.has_band_k_diag
+    )
 end
 
 function is_s_diagonalizable(L::AbstractMatrix{<:Integer}, S::Tuple{Vararg{Integer}})
     _assert_matrix_is_undirected_laplacian(L)
     return has_s_bandwidth_at_most_k(L, S, size(L, 1))
+end
+
+"""
+    _s_spectra_has_bandwidth_at_most_k(spec, k) -> SBandRecognitionResult
+
+[TODO: Write here. Also, comment inline]
+"""
+function _s_spectra_has_bandwidth_at_most_k(spec::SSpectra, k::Integer)
+    L = copy(spec.matrix)
+    S = spec.S
+
+    if !spec.s_diagonalizable
+        return SBandRecognitionResult(L, S, nothing, k, false)
+    end
+
+    n = size(spec.matrix, 1)
+    eigvecs = Matrix{Int}(undef, n, n)
+
+    multiplicities = spec.multiplicities
+    s_eigenspaces = spec.s_eigenspaces
+    s_eigenbases = spec.s_eigenbases
+
+    res = iterate(multiplicities)
+    k_basis_found = true
+    idx = 1
+
+    while if k_basis_found
+        idx <= n
+    end
+        ((eigval, multi), state) = res
+        k_basis = find_k_orthogonal_basis(
+            s_eigenspaces[eigval], multi, k, s_eigenbases[eigval]
+        )
+
+        if isnothing(k_basis)
+            k_basis_found = false
+        else
+            eigvecs[:, idx:(idx + multi - 1)] = k_basis
+            res = iterate(multiplicities, state)
+            idx += multi
+        end
+    end
+
+    if k_basis_found
+        eigvals = vcat(fill.(keys(multiplicities), values(multiplicities))...)
+        diagonalization = Eigen(eigvals, eigvecs)
+        has_band_k_diag = true
+    else
+        diagonalization = nothing
+        has_band_k_diag = false
+    end
+
+    return SBandRecognitionResult(L, S, diagonalization, k, has_band_k_diag)
 end
